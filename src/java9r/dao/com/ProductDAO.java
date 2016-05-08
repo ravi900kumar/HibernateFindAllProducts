4cf070b315352d7765729f12197e82be7fba6f82
Ravi <Ravi@10.0.0.5>
2016-05-09 00:43:04 +0530

    ravi

---
+++ src/java9r/dao/com/ProductDAO.java
@@ -0,0 +1,29 @@
+package java9r.dao.com;
+
+import java9r.entites.com.*;
+import java9r.util.com.*;
+import org.hibernate.*;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.type.IntegerType;
+import org.hibernate.type.Type;
+
+import java.util.*;
+
+public class ProductDAO {
+	private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+	@SuppressWarnings("unchecked")
+	public List<Product> FindAllProducts() {
+		try {
+			if (!sessionFactory.getCurrentSession().getTransaction().isActive())
+				sessionFactory.getCurrentSession().getTransaction().begin();
+			return sessionFactory.getCurrentSession().createCriteria(Product.class).list();
+
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+}
